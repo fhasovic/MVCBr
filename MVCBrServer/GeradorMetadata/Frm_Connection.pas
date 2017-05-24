d47cc940e5c9340038704fa63c02b9aa05728d60
Amarildo Lacerda <amarildo_51@msn.com>
2017-05-23 23:35:27 -0300

    -+ conversão para estrutura do MVCBr.

--- MVCBrServer/GeradorMetadata/Frm_Connection.pas
+++ MVCBrServer/GeradorMetadata/Frm_Connection.pas
@@ -3,12 +3,19 @@ unit Frm_Connection;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Frm_Principal,
-  Vcl.ExtCtrls, Vcl.DBCtrls, Vcl.Mask, Vcl.Buttons,db;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, {Frm_Principal,}
+  MVCBr.Interf, MVCBr.FormView,
+  Vcl.ExtCtrls, Vcl.DBCtrls, Vcl.Mask, Vcl.Buttons, db;
 
 type
-  TFrmConnection = class(TForm)
+
+  IFrmConnection = Interface(IView)
+    ['{9DC4299F-FFC9-464D-8F59-8B317D5DC8C3}']
+  End;
+
+  TFrmConnection = class(TFormFactory, IFrmConnection)
     GroupBox1: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
@@ -43,7 +50,6 @@ type
     { Public declarations }
   end;
 
-
 var
   FrmConnection: TFrmConnection;
 
@@ -51,43 +57,56 @@ implementation
 
 {$R *.dfm}
 
+uses GeradorMetadata.Controller.Interf;
+
 procedure TFrmConnection.FormDeactivate(Sender: TObject);
+var ctrController:IGeradorMetadataController;
+
 begin
-   FrmPrincipal.CDS_CNN.Cancel;
-   Close;
+
+  ctrController := resolveController<IGeradorMetadataController>;
+  ctrController.CDS_CNN_cancel;
+
+  //FrmPrincipal.CDS_CNN.Cancel;
+  Close;
 end;
 
 procedure TFrmConnection.SpeedButton1Click(Sender: TObject);
 begin
   if OD.Execute() then
-  DBEdit3.Text := OD.FileName;
+    DBEdit3.Text := OD.FileName;
 end;
 
 procedure TFrmConnection.SpeedButton2Click(Sender: TObject);
 begin
   if OD.Execute() then
-  DBEdit7.Text := OD.FileName;
+    DBEdit7.Text := OD.FileName;
 end;
 
 procedure TFrmConnection.btnCancelClick(Sender: TObject);
 begin
-   FrmPrincipal.CDS_CNN.Cancel;
-   Close;
+  //FrmPrincipal.CDS_CNN.Cancel;
+  with resolveController<IGeradorMetadataController> do
+      CDS_CNN_cancel;
+
+  Close;
 end;
 
 procedure TFrmConnection.btnSalvarClick(Sender: TObject);
 begin
-   FrmPrincipal.CDS_CNN.Post;
-   Close;
+  with resolveController<IGeradorMetadataController> do
+       CDS_CNN_Post;
+ // FrmPrincipal.CDS_CNN.Post;
+  Close;
 end;
 
 procedure TFrmConnection.DBComboBox1Change(Sender: TObject);
 begin
-   Label7.Visible := DBComboBox1.ItemIndex in [ DBComboBox1.Items.IndexOf('Firebird'),
-                                                DBComboBox1.Items.IndexOf('MySQL'),
-                                                DBComboBox1.Items.IndexOf('Interbase'),
-                                                DBComboBox1.Items.IndexOf('PostgreSQL')];
-   DBEdit6.Visible := Label7.Visible;
+  Label7.Visible := DBComboBox1.ItemIndex
+    in [DBComboBox1.Items.IndexOf('Firebird'),
+    DBComboBox1.Items.IndexOf('MySQL'), DBComboBox1.Items.IndexOf('Interbase'),
+    DBComboBox1.Items.IndexOf('PostgreSQL')];
+  DBEdit6.Visible := Label7.Visible;
 end;
 
 end.
--- MVCBrServer/GeradorMetadata/ORMBrGeneratorModel.dpr
+++ MVCBrServer/GeradorMetadata/ORMBrGeneratorModel.dpr
@@ -1,16 +1,26 @@
 program ORMBrGeneratorModel;
 
-
-
 uses
   Forms,
+  MVCBr.ApplicationController,
   Frm_Principal in 'Frm_Principal.pas' {FrmPrincipal},
-  Frm_Connection in 'Frm_Connection.pas' {FrmConnection};
+  Frm_Connection in 'Frm_Connection.pas' {FrmConnection},
+  GeradorMetadata.Controller.Interf in 'Controllers\GeradorMetadata.Controller.Interf.pas',
+  GeradorMetadata.Controller in 'Controllers\GeradorMetadata.Controller.pas',
+  GeradorMetadataConnection.Controller.Interf in 'Controllers\GeradorMetadataConnection.Controller.Interf.pas',
+  GeradorMetadataConnection.Controller in 'Controllers\GeradorMetadataConnection.Controller.pas';
 
 {$R *.res}
 
 begin
-  Application.Initialize;
-  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
-  Application.Run;
+  // Application.Initialize;
+  // Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  // Application.Run;
+
+  ApplicationController.run( TGeradorMetadataController.New  ,
+    function: boolean
+    begin
+       result := true;
+    end);
+
 end.
